698da58fe81bb3da4a105d1265706d4bc88d7aec
groxyash <yashgoyal.ec@gmail.com>
2025-12-30 14:58:38 +0530

    Initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        AuthenSight AI — Tailwind Fixed ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
