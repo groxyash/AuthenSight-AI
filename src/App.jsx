009c3b330344b940d230dd44bc06888cad65b3a5
Aman0728 <mcool3807@gmail.com>
2026-01-04 20:07:15 +0530

    About page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,32 @@ import './App.css'
 import Home from './pages/Home.jsx'
 import Reports from './pages/Reports.jsx'
 import About from './pages/About.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import ProfileLayout from './ProfileLayout.jsx'
+import Profile from './compnents/About/profile.jsx'
+import { createBrowserRouter, RouterProvider ,createRoutesFromElements, Route,} from 'react-router-dom'
 
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element:<Home/>
-    },
-    {
-      path: "/about",
-      element:<About/>
-    },
-    {
-      path: "/reports",
-      element:<Reports/>
-    },
-  ])
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+  <>
+      {/* ROUTES WITH NAVBAR */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="reports" element={<Reports />} />
+      </Route>
+
+      {/* ROUTES WITHOUT NAVBAR */}
+      <Route element={<ProfileLayout />}>
+        <Route path="profile/:id" element={<Profile />} />
+      </Route>
+    </>
+
+  )
+  )
   return (
     <>
     <RouterProvider router = {router} />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,36 @@
 import NavBar from '../compnents/NavBar.jsx'
 import TextInput from '../compnents/TextInput.jsx'
+import Card from '../compnents/About/Card/Card.jsx';
+import "../compnents/About/Card/Card.css";
 
 function About() {
   return(
-    <></>
+    <div className="cards-container">
+      <Card
+        imgSrc="..."
+        title="About Project"
+        subtitle="React"
+        description="Some text"
+        profileId="Aman"
+      />
+      <Card
+        imgSrc="..."
+        title="About Project"
+        subtitle="React"
+        description="Some text"
+        profileId="Rishik"
+      />
+      <Card
+        imgSrc="..."
+        title="About Project"
+        subtitle="React"
+        description="Some text"
+        profileId="Yash"
+      />
+
+
+      
+    </div>
   )
 }
 
